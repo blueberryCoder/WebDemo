d04a7e1c94dc0ac0b9cc6a82c39833aaac1cdafa
blueberry <992036795@qq.com>
2016-11-14 16:39:49 +0800

    delete statnder.jar jstl.jar

--- Spittr/src/test/com/blueberry/spittr/controller/HomeControllerTest.java
+++ Spittr/src/test/com/blueberry/spittr/controller/HomeControllerTest.java
@@ -1,14 +1,22 @@
 package com.blueberry.spittr.controller;
 
 
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 /**
  * Created by Administrator on 2016/11/9.
  */
-public class HomeControllerTest  {
+public class HomeControllerTest {
 
 
-    public void testHomePage(){
+    public void testHomePage() throws Exception {
 
         HomeController controller = new HomeController();
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home"))
     }
 }
---
+++ Spittr/src/test/com/blueberry/spittr/controller/SpitterControllerTest.java
@@ -0,0 +1,18 @@
+package com.blueberry.spittr.controller;
+
+import com.blueberry.spittr.beans.Spitter;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/11/14.
+ */
+public class SpitterControllerTest {
+
+    @Test
+    public void shouldShowPagedSpittles() throws Exception {
+    }
+
+}
